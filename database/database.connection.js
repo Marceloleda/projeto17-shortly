05fd59d71fc98201425f87c22ddb750fd93fc0ae
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2023-03-03 17:58:23 -0400

    alguns ajustes

--- database/database.connection.js
+++ database/database.connection.js
@@ -6,9 +6,12 @@ const { Pool } = pg;
 
 const configDatabase = {
   connectionString: process.env.DATABASE_URL,
+  ...(process.env.NODE_ENV === "production" && {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }),
 };
 
-if (process.env.MODE === "prod") configDatabase.ssl = true;
-console.log("conectado ao Postgres")
-
 export const db = new Pool(configDatabase);
+
